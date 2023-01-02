5bc6f1b020853c3aad3dc7079b4a64a223e5f905
Billy Keyes <bluekeyes@gmail.com>
2023-01-01 21:23:27 -0800

    Add example USB callbacks
    This is mostly copied from the examples for now, but I modified the
    string descriptor callback to return an accurate serial number.

---
+++ src/usb_descriptors.h
@@ -0,0 +1,4 @@
+#ifndef USB_DESCRIPTORS_H_
+#define USB_DESCRIPTORS_H_
+
+#endif /* USB_DESCRIPTORS_H_ */
---
+++ src/usb_hid.c
@@ -0,0 +1,23 @@
+#include "tusb.h"
+
+// Invoked when received GET_REPORT control request
+// Application must fill buffer report's content and return its length.
+// Return zero will cause the stack to STALL request
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen) {
+  (void) instance;
+  (void) report_id;
+  (void) report_type;
+  (void) buffer;
+  (void) reqlen;
+  return 0;
+}
+
+// Invoked when received SET_REPORT control request or
+// received data on OUT endpoint ( Report ID = 0, Type = 0 )
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) {
+  (void) instance;
+  (void) report_id;
+  (void) report_type;
+  (void) buffer;
+  (void) bufsize;
+}
