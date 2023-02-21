da108db56bd307bed2f21fc502b401875fdd446a
Billy Keyes <bluekeyes@gmail.com>
2023-02-20 21:33:30 -0800

    Fix warnings in unix device backend

--- cli/src/device/unix.rs
+++ cli/src/device/unix.rs
@@ -3,11 +3,11 @@ use crate::device::{Error, Report};
 pub struct Device {}
 
 impl Device {
-    pub fn open(vendor_id: u16, product_id: u16) -> Result<Device, Error> {
+    pub fn open(_vendor_id: u16, _product_id: u16) -> Result<Device, Error> {
         unimplemented!();
     }
 
-    pub fn send_report(&self, report: Report) -> Result<(), Error> {
+    pub fn send_report(&self, _report: Report) -> Result<(), Error> {
         unimplemented!();
     }
 }
