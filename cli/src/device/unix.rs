4857f86f6f85cfa93551fba5bc6245c26c8d1df3
Billy Keyes <bluekeyes@gmail.com>
2023-02-22 23:05:55 -0800

    Add read_temperature stub for unix device backend

--- cli/src/device/unix.rs
+++ cli/src/device/unix.rs
@@ -4,10 +4,14 @@ pub struct Device {}
 
 impl Device {
     pub fn open(_vendor_id: u16, _product_id: u16) -> Result<Device, Error> {
-        unimplemented!();
+        unimplemented!()
     }
 
     pub fn send_report(&self, _report: Report) -> Result<(), Error> {
-        unimplemented!();
+        unimplemented!()
+    }
+
+    pub fn read_temperature(&self) -> Result<i16, Error> {
+        unimplemented!()
     }
 }
